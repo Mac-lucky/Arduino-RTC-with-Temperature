77d7cccc9d438d11e5c58372df4f1fc6d733797b
gryzmol <gryzmol@op.pl>
2020-09-13 13:05:29 +0200

    PIR activated LCD backlight

--- RTC.ino
+++ RTC.ino
@@ -1,6 +1,7 @@
 #include <Adafruit_BME280.h>
 #include <DS3231.h>
 #include <LiquidCrystal.h>
+#define PIR 11
 
 Adafruit_BME280 bme; // use I2C interface
 
@@ -15,14 +16,12 @@ int but2=1;// pin 1 for Minutes
 
 DS3231  rtc(SDA, SCL);
 LiquidCrystal lcd(2, 3, 4, 5, 6, 7);
-const int Time_light=999;
-int bl_TO=Time_light;// Backlight Time-Out
 int bl=10; // Backlight pin
 const int backlight=120; // no more then 7mA !!!
 
 
 void setup() {   
-
+    pinMode(PIR, INPUT);
     bme.begin();
     rtc.begin();
     //rtc.setTime(12, 0, 0);     // Set the time to 12:00:00 (24hr format)
@@ -32,15 +31,13 @@ void setup() {
     lcd.begin(16,2);
     pinMode(but1,INPUT_PULLUP);// avoid external Pullup resistors for Button 1
     pinMode(but2,INPUT_PULLUP);// and Button 2
-    analogWrite(bl,backlight);// Turn on Backlight
+    analogWrite(bl,0);// Turn on Backlight
 }
 void loop() { 
-
 for (int a=0; a<8; a++) 
 {
-    if(digitalRead(1) == LOW){
-        lottery();
-    }
+    but2press();
+    turnonpir();
     lcd.setCursor(0,0);
     lcd.print(rtc.getDOWStr(FORMAT_SHORT));
     lcd.print(" ");
@@ -54,6 +51,8 @@ lcd.clear();
 
 for (int b=0; b<8; b++)
 {
+    but2press();
+    turnonpir();
     lcd.setCursor(0,0);
     lcd.print(bme.readTemperature());
     lcd.print("*C   ");
@@ -92,5 +91,29 @@ lcd.setCursor(3, 0);
     }
 delay(50);    
 }    
-delay(5000);
+delay(7000);
 }
+
+void turnonpir(){
+    if (digitalRead(PIR) == HIGH){
+        analogWrite(bl,backlight);
+    }
+    else{
+        analogWrite(bl,0);
+    }
+}
+
+void but2press(){
+    if(digitalRead(1) == LOW){
+        analogWrite(bl,backlight);
+        lottery();
+    }
+}
+
+void but1press(){
+    if(digitalRead(0) == LOW){
+        analogWrite(bl,backlight);
+        lottery();
+    }
+}
+
